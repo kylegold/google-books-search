730a363d444bdb05578ee879fdd6595a8c66b83b
kyle <kyle@kyle.nyc>
2020-12-08 19:45:07 -0500

    ran create-react-app.. created setup for saved and search components and added react routes in app.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import Saved from './components/Saved';
+import Search from './components/Search';
+
+function App() {
+  return (
+   <Router>
+    <div className="App">
+       <Route exact path="/" component={Search} />
+       <Route exact path="/search" component={Search} />
+       <Route exact path="/saved" component={Saved} />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ components/Saved.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Saved = () => {
+  return (
+    <p>Saved</p>
+  )
+}
+
+export default Saved;
